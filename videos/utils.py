04ee23df9baf527deea44adabfd8e765fd6a9330
Kendrick Ledet <kendrickledet@gmail.com>
2012-06-24 10:00:13 -0500

    improved some comments

--- videos/utils.py
+++ videos/utils.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 # import MEDIA_ROOT folder to store videos in
-from   emp.settings import MEDIA_ROOT
+from   emp.settings  import MEDIA_ROOT
 # import Video model
 from   videos.models import Video
 """
@@ -13,18 +13,21 @@ TODO: Complete slug functionality, remember to have slug update when the title i
 Workflow:
 	'title','description','categories','nsfw','source_file' have been filled out via submitted form
 	upload_datetime is filled out automatically
-	The uploaded file is attributed to the 'source_file' field and is uploaded to "videos/src/"
+	The source file is attributed to the 'source_file' field and is uploaded to "videos/src/"
+	The source file is passed into convert_uploaded_video() for further processing
 """
 def process_uploaded_video(uploaded_video):
+	# Get some basic file info
 	filename = str(uploaded_video.source_file.name)
 	filesize = uploaded_video.source_file.size
-	## TODO: Fill in methods to commit complete Video Model instance
-	# uploaded_video.uploader 	 =
+
+	# Begin filling out Video model data 
 	uploaded_video.converted	 = False
 	uploaded_video.rating		 = 0
-	# uploaded_video.vidtype	 =
 	uploaded_video.favorites     = 0
 	uploaded_video.views		 = 0
+	# uploaded_video.uploader 	 =
+	# uploaded_video.vidtype	 =
 
 	# Create video title slug for usage in URLs
 	title_slug					 = str(uploaded_video.title).lower()
@@ -41,7 +44,7 @@ def process_uploaded_video(uploaded_video):
 Utility functions to pass uploaded video data into ffmpeg/mencoder to be converted, as well as
 into the utilities necessary to inject metadata (yamdi, flvtool2) and generate thumbnails (ffmpegthumbnailer?)
 
-Also, update additional Video object model down here
+Also, update additional Video object model properties down here
 
 TODO: 
 	Implement FFMPEG/mencoder, flvtool2, and perhaps Celery for process handling
@@ -49,26 +52,22 @@ TODO:
 	Source file is deleted by default as of now: allow settings to require otherwise,
 		which will prove useful when the HTML5 functionality is added and multiple
 		output formats will be used
+	Add functionality to allow for admins to re-upload source files and re-convert
 """
 def convert_uploaded_video(filename, uploaded_video):
-	video_id	  = uploaded_video.id
-
+	# Get video id to use for filename
+	video_id	= uploaded_video.id
+	# Set source and destination paths for ffmpeg (and other tools)
 	src_path    = MEDIA_ROOT + '/videos/src/' + filename
 	dest_path	= MEDIA_ROOT + '/videos/flv/' + str(video_id) + '.flv'
-
-	# convert file to .flv using ffmpeg
-	# store in media/videos/
+	# Convert file to .flv using ffmpeg
 	ffmpeg_call = "ffmpeg -i "+ src_path +" -ar 22050 -ab 96k -r 24 -b 600k -f flv " + dest_path
 	os.system(ffmpeg_call)
-	# proc = subprocess.call(['ffmpeg', ffmpeg_args])
-
 	# Delete sauce file
 	call = subprocess.call(['rm', str(src_path)])
-
-	uploaded_video.length 	     = get_video_length(dest_path)
-	# Commit data to Video object model in db #
+	# Commit additional data to Video object model in db #
+	uploaded_video.length 	      = get_video_length(dest_path)
 	uploaded_video.converted_file = dest_path
-	# Add functionality to allow for admins to re-upload source files and re-convert
 	uploaded_video.source_file    = ""
 	uploaded_video.converted  	  = True
 	uploaded_video.save()
