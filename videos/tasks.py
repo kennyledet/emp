1bb32290cce9de991d3cc455214272a080e95b5a
Kendrick Ledet <kendrickledet@gmail.com>
2012-06-25 17:29:11 -0500

    ..

--- videos/tasks.py
+++ videos/tasks.py
@@ -2,13 +2,12 @@ from celery.task     import Task
 from celery.registry import tasks
 from videos.utils    import convert_uploaded_video
 """
-Task to be ran from the video_upload view to process video upload 
+Task to be started from videos.views.video_upload to process uploaded video
+Process contained in videos.utils
 """
 class ProcessVideoTask(Task):
 	def run(self, video_id, **kwargs):
-		# CONVERT VIDEO, pass in Video object for further processing
+		# Convert video, pass in Video object for further processing
 		convert_uploaded_video(video_id)
-
-	
 
 tasks.register(ProcessVideoTask)
