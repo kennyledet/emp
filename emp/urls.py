d62ac74e4cfcf556932c19f02b752dbcdcdc900e
Kendrick Ledet <kendrickledet@gmail.com>
2012-06-25 15:08:48 -0500

    starting integrating flowplayer / basic video play page placeholder

--- emp/urls.py
+++ emp/urls.py
@@ -1,4 +1,6 @@
+from django.conf      import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -24,4 +26,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
