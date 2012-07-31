633d5a29710ecd1e919f22bf6238809e4af40e1c
Kendrick Ledet <kendrickledet@gmail.com>
2012-07-31 11:09:11 -0500

    removed profiles app

--- emp/apps/profiles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf      import settings
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('emp.apps.profiles.views',
-    url(r'^(?P<username>[A-Za-z0-9_]*)/$', 'user_profile', name='profile'),
-
-    # url(r'^$',       'accounts.views.profiles', name='profiles'),
-
-    )
